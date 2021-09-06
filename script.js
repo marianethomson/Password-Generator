fc203f0c7fc89cef5d944f55995ea42ebd4ba87b
Mariane Thomson <marianedinis@gmail.com>
2021-09-07 00:18:27 +1000

    work in progress js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ from pool math.random for each character
 
 display in the page*/
 
-//chnaging to getElementByID as I just want to return the specific button to generate
+//changing to getElementByID as I just want to return the specific button to generate
 var generateBtn = document.getElementById("generate");
 
 //Array of lowercase characters
@@ -47,8 +47,6 @@ var arrLowerChar = [
 // Array of Uppercase using map to run elements and toUpperCase to manipulate string
 var arrUpperChar = arrLowerChar.map((letter) => letter.toUpperCase());
 
-console.log(arrUpperChar);
-
 // Array of special characters to be included in password
 var arrSymbol = [
   "@",
@@ -79,10 +77,65 @@ var arrSymbol = [
 // Array of numbers
 var arrNumber = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
+//check pw size
+/* function checkPwSize() {
+  let requiredSize = document.getElementById("pw-size");
+  if (requiredSize < 5 || requiredSize > 128) {
+    let errorMsg = document.createElement("div");
+    errorMsg.textContent(
+      "The specified size does not meet the password requirements."
+    );
+    errorMsg.appendChild(this);
+    errorMsg.setAttribute("style", "color: red");
+  }
+} */
+
+//error message if pw doesn't meet minimum requirements
+/* function checkRequirements() {
+  let arrayCheckbox = document.querySelectorAll("checkbox");
+  for (i=0, i<arrayCheckbox, i++){
+    (if arrayCheckbox[i]=true){
+      
+    }
+  }
+} */
+
+//defines pw size
+var pwSize = document.getElementById("pw-size");
+
+//initializes array to receive pw requirements type
+var pwPool = [];
+
+//get values from the checkbox number and push array type into the pw pool
+if ((document.getElementById("number").checked = true)) {
+  pwPool.push(arrNumber);
+}
+
+//get values from the checkbox symbol and push array type into the pw pool
+if ((document.getElementById("symbol").checked = true)) {
+  pwPool.push(arrSymbol);
+}
+
+//get values from the checkbox upp-case and push array type into the pw pool
+if ((document.getElementById("upp-case").checked = true)) {
+  pwPool.push(arrUpperChar);
+}
+
+//get values from the checkbox low-case and push array type into the pw pool
+if ((document.getElementById("low-case").checked = true)) {
+  pwPool.push(arrLowerChar);
+}
+
+//transforms multi-level array into flat array
+pwPool.flat;
+
+//function to random select elements from the pwPool
+//up to the lenght required
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document. ("#password");
+  var passwordText = document.getElementsByClassName("#password");
 
   passwordText.value = password;
 }
