c9c611c2183d07ab9b6e7ddfacb8d3ff110bf36e
Mariane Thomson <marianedinis@gmail.com>
2021-09-10 01:37:49 +1000

    final files

--- script.js
+++ script.js
@@ -67,133 +67,85 @@ var arrSymbol = [
 // Array of numbers
 var arrNumber = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
-//initializes array to receive pw requirements type
-var pwPool = [];
-
-//creates an empty string to receive pw
-var password = "";
-
-//variable to store input checkbox
-var numberCheckbox = document.querySelector("#number");
-
-//variable to store input checkbox
-var symbolCheckbox = document.querySelector("#symbol");
-
-//variable to store input checkbox
-var uppCaseCheckbox = document.querySelector("#upp-case");
-
-//variable to store input checkbox
-var lowCaseCheckbox = document.querySelector("#low-case");
-
-//variable to store password size
-var pwSize = document.querySelector("#pw-size");
-
-//defines the requested pw size as string and converts back into int. Return pwSize for random
-function getPWSize() {
-  pwSize = parseInt(pwSize, 10);
-  return pwSize;
-}
-
-//get values from the checkbox number and push array type into the pw pool
-function includeNumber() {
-  if ((numberCheckbox.checked = true)) {
-    pwPool.push(arrNumber);
-  }
-}
-
-//get values from the checkbox symbol and push array type into the pw pool
-function includeSymbol() {
-  if ((symbolCheckbox.checked = true)) {
-    pwPool.push(arrSymbol);
-  }
-}
-
-//get values from the checkbox upp-case and push array type into the pw pool
-function includeUppCase() {
-  if ((uppCaseCheckbox.checked = true)) {
-    pwPool.push(arrUpperChar);
-  }
-}
-
-//get values from the checkbox low-case and push array type into the pw pool
-function includeLowCase() {
-  if ((lowCaseCheckbox.checked = true)) {
-    pwPool.push(arrLowerChar);
-  }
-}
-
-// function to check if at least one type of requirement was selected
-function checkRequirements() {
-  if (
-    numberCheckbox === false &&
-    symbolCheckbox === false &&
-    uppCaseCheckbox === false &&
-    lowCaseCheckbox === false
-  ) {
-    alert("You need to select at least one type of password element");
-  }
-}
-
-//function to run requirements and include them into an array. Multi dimensional goes into flat array. Return array to random
-function includePassword() {
-  pwPool = includeNumber();
-  pwPool = includeSymbol();
-  pwPool = includeUppCase();
-  pwPool = includeLowCase();
-  pwPool = pwPool.flat;
-  return pwPool;
-}
-
-//randomizing elements in the pw and returning the randomized array
-function generatePassword() {
-  pwSize = getPWSize();
-  pwPool = includePassword();
-  pwPool = pwPool[Math.floor(Math.random() * pwSize)];
-  return pwPool;
-}
-
-//function to change array back into string and removing commas
-function passToString() {
-  password = generatePassword();
-  password = password.toString.replace(/,/g, "");
-  return password;
-}
-
-// Write password to the #password input
-function writePassword() {
-  var passwordText = passToString();
-  passwordText.document.createElement("#password");
-  body.appendChild(textarea);
-}
-
-//function to copy pw to clipboard
-function copyToClipboard() {
-  var copyPW = document.querySelector("#password");
-  copyPW.select();
-  copyPW.setSelectionRange(0, 99999);
-  navigator.clipboard.writeText(copyPW.value);
-}
+//array to shuffle elements
+var tempArray = [];
 
 //clear local storage
 function clearForm() {
-  localStorage.removeItem("pw-size");
-  localStorage.removeItem("number");
-  localStorage.removeItem("symbol");
-  localStorage.removeItem("upp-case");
-  localStorage.removeItem("low-case");
+  localStorage.removeItem("password");
+}
+
+//Added feature: function to copy pw to clipboard
+function copyToClipboard() {
+  var copyPW = document.querySelector("#password");
+  copyPW.select();
+  copyPW.setSelectionRange(0, 127);
+  navigator.clipboard.writeText(copyPW.value);
 }
 
 // Add event listener to generate button and saves input into localStorage
-generateBtn.addEventListener("click", function () {
-  preventDefault(), localStorage.setItem("pw-size", pwSize.value);
-  localStorage.setItem("number", numberCheckbox.checked);
-  localStorage.setItem("symbol", symbolCheckbox.checked);
-  localStorage.setItem("upp-case", uppCaseCheckbox.checked);
-  localStorage.setItem("low-case", lowCaseCheckbox.checked);
-  checkRequirements(), writePassword();
+generateBtn.addEventListener("click", function (event) {
+  //inhibit form submit
+  event.preventDefault();
+  //creates obj password
+  password = {
+    size: document.getElementById("pw-size").value.trim(),
+    number: document.getElementById("number").checked,
+    symbol: document.getElementById("symbol").checked,
+    uppCase: document.getElementById("upp-case").checked,
+    lowCase: document.getElementById("low-case").checked,
+  };
+  //checks size requirement and if not met delete obj
+  if (Number(password.size) < 8 || Number(password.size) > 128) {
+    alert(
+      "The password requires a minumum of 8 characters and a maximum of 128 characters."
+    );
+    clearForm();
+  }
+  //checks pw elements and if not met delete obj
+  else if (
+    password.number == false &&
+    password.symbol == false &&
+    password.uppCase == false &&
+    password.lowCase == false
+  ) {
+    alert("You need to select at least one type of password element.");
+    clearForm();
+  }
+  //if requirements met
+  else {
+    //var to store the calculated password
+    var pwContent = [];
+    //save obj as string
+    localStorage.setItem("password", JSON.stringify(password));
+    // if checkbox true, populate temporary array with numbers
+    if (password.number == true) {
+      tempArray = tempArray.concat(arrNumber);
+    }
+    // if checkbox true, populate temporary array with symbols
+    if (password.symbol == true) {
+      tempArray = tempArray.concat(arrSymbol);
+    }
+    // if checkbox true, populate temporary array with upper case
+    if (password.uppCase == true) {
+      tempArray = tempArray.concat(arrUpperChar);
+    }
+    // if checkbox true, populate temporary array with lower case
+    if (password.lowCase == true) {
+      tempArray = tempArray.concat(arrLowerChar);
+    }
+    //random array with password size, where each element is picked from a random
+    //index pickup from tempArray using math random
+    for (let i = 0; i < password.size; i++) {
+      var randomElement = tempArray[Math.floor(Math.random() * password.size)];
+      pwContent.push(randomElement);
+    }
+    var passwordText = document.getElementById("password");
+    passwordText.value = pwContent;
+  }
 });
 
 //Add event listener to the reset button
-generateBtn.addEventListener("click", function () {
-  document.body.onload = clearForm();
+resetBtn.addEventListener("click", function () {
+  clearForm();
 });
